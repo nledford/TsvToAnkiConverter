2b5f2d635bbdb9a3fe50c5a7a5780b9c6876c6fd
Nathaniel Ledford <nate@nateledford.com>
2021-09-01 14:26:01 -0400

    Call function to create anki decks

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{env, process};
 
 use anyhow::Result;
 
-use jplt_anki::tsv;
+use jplt_anki::{anki, tsv};
 
 fn main() -> Result<()> {
     env::set_var("RUST_BACKTRACE", "full");
@@ -14,6 +14,16 @@ fn main() -> Result<()> {
             process::exit(1)
         }
     };
+
+    match anki::create_anki_decks(levels) {
+        Ok(_) => {
+            println!("Decks created successfully!")
+        },
+        Err(err) => {
+            println!("Error occurred while creating anki decks: {}", err);
+            process::exit(1)
+        }
+    }
 
     println!("\n\nDone!");
 
